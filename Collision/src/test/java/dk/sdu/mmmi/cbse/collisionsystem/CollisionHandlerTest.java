87e96bc51d850bdbdc2b7ff4a9aca22afb4783ef
MacBech <115424555+MacBech@users.noreply.github.com>
2024-05-17 18:33:20 +0200

    Setup collision test class

---
+++ Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionHandlerTest.java
@@ -0,0 +1,17 @@
+package dk.sdu.mmmi.cbse.collisionsystem;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CollisionHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void collision() {
+    }
+}
