f5665ba359d3efddbc7faf35f1c79bba4f2001e5
MacBech <115424555+MacBech@users.noreply.github.com>
2024-05-21 13:02:42 +0200

    Working collision test

--- Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionHandlerTest.java
+++ Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionHandlerTest.java
@@ -1,5 +1,6 @@
 package dk.sdu.mmmi.cbse.collisionsystem;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,11 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CollisionHandlerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
 
     @Test
     void collision() {
+        CollisionHandler collisionHandler = new CollisionHandler();
+        Entity entity1 = new Entity();
+        Entity entity2 = new Entity();
+
+        entity1.setRadius(5);
+        entity2.setRadius(5);
+
+        // Both entities will have their x and y coordinates set to 0.0
+
+        boolean collision = collisionHandler.collision(entity1, entity2);
+
+        assertTrue(collision);
     }
 }
